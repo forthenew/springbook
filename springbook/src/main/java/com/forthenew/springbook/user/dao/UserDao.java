ca989538045ddeb67ae63544c0545192afd3868d
forthenew <forthenew@forthenew-DeskTop>
2016-05-02 23:00:57 +0900

    added test code

--- springbook/src/main/java/com/forthenew/springbook/user/dao/UserDao.java
+++ springbook/src/main/java/com/forthenew/springbook/user/dao/UserDao.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+
 import com.forthenew.springbook.user.User;
 
 public class UserDao {
@@ -17,7 +19,7 @@ public class UserDao {
 	}
 	
 	public void add(User user) throws SQLException {
-		Connection c = this.dataSource.getConnection();
+		Connection c = dataSource.getConnection();
 		
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 		ps.setString(1, user.getId());
@@ -30,20 +32,52 @@ public class UserDao {
 	}
 	
 	public User get(String id) throws SQLException {
-		Connection c = this.dataSource.getConnection();
+		Connection c = dataSource.getConnection();
 		
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
 		ResultSet rs = ps.executeQuery();
-		rs.next();
-		User user = new User();
-		user.setId(rs.getString("id"));
-		user.setName(rs.getString("name"));
-		user.setPassword(rs.getString("password"));
+		User user = null;
+		if(rs.next()) {
+			user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+		}
 		
 		rs.close();
 		ps.close();
 		c.close();
+		
+		if(user == null) throw new EmptyResultDataAccessException(1);
+		
 		return user;
+	}
+	
+	public void deleteAll() throws SQLException {
+		Connection c = dataSource.getConnection();
+		
+		PreparedStatement ps = c.prepareStatement("DELETE FROM users");
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+	}
+	
+	public int getCount() throws SQLException {
+		Connection c = dataSource.getConnection();
+		int count;
+		
+		PreparedStatement ps = c.prepareStatement("SELECT COUNT(*) FROM users");
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		count = rs.getInt(1);
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return count;
+		
 	}
 }
--- springbook/src/test/java/com/forthenew/springbook/user/dao/UserDaoTest.java
+++ springbook/src/test/java/com/forthenew/springbook/user/dao/UserDaoTest.java
@@ -1,35 +1,70 @@
 package com.forthenew.springbook.user.dao;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.sql.SQLException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.forthenew.springbook.user.User;
 
 public class UserDaoTest {
-	Logger logger = LoggerFactory.getLogger(UserDaoTest.class);
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	private UserDao dao;
+	private User user1;
+	private User user2;
+	private User user3;
+	
+	@Before
+	public void setup() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("/spring/application-config.xml");
 //		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		UserDao userDao = context.getBean("userDao", UserDao.class);
+		dao = context.getBean("userDao", UserDao.class);
+		user1 = new User("gyumee", "박성철", "springno1");
+		user2 = new User("leegw700", "이길원", "springno2");
+		user3 = new User("bumjin", "박범진", "springno3");
+	}
+	@Test
+	public void addAndGet() throws SQLException {
 		
-		User user = new User();
-		user.setId("whiteship");
-		user.setName("백기선");
-		user.setPassword("married");
+		dao.deleteAll();
+		assertThat(new Integer(0), is(dao.getCount()));
 		
-		userDao.add(user);
+		dao.add(user1);
+		dao.add(user2);
+		assertThat(new Integer(2), is(dao.getCount()));
 		
-		System.out.println(user.getId() + " 등록 성공");
+		User userget1 = dao.get(user1.getId());
+		assertThat(user1.getPassword(), is(userget1.getPassword()));
 		
-		User user2 = userDao.get(user.getId());
-		System.out.println(user2.getName());
-		System.out.println(user2.getPassword());
-		System.out.println(user2.getId() + " 조회 성공");
+		User userget2 = dao.get(user2.getId());
+		assertThat(user2.getPassword(), is(userget2.getPassword()));
+	}
+	
+	@Test
+	public void count() throws SQLException {
+		dao.deleteAll();
+		assertThat(new Integer(0), is(dao.getCount()));
+		
+		dao.add(user1);
+		assertThat(new Integer(1), is(dao.getCount()));
+		
+		dao.add(user2);
+		assertThat(new Integer(2), is(dao.getCount()));
+		
+		dao.add(user3);
+		assertThat(new Integer(3), is(dao.getCount()));
+	}
+	
+	@Test(expected=EmptyResultDataAccessException.class)
+	public void getUserFailure() throws SQLException {
+		dao.deleteAll();
+		assertThat(new Integer(0), is(dao.getCount()));
+
+		dao.get("unkonwn_id");
 	}
 }
