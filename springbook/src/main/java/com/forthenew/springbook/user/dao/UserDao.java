b1bd27fd64a4a1bc8f4f7ff01f38ef9682e4a6ab
forthenew <forthenew@gmail.com>
2016-05-03 08:51:11 +0900

    DataSource 인스턴스 변수에서 static 제거

--- springbook/src/main/java/com/forthenew/springbook/user/dao/UserDao.java
+++ springbook/src/main/java/com/forthenew/springbook/user/dao/UserDao.java
@@ -12,7 +12,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import com.forthenew.springbook.user.User;
 
 public class UserDao {
-	private static DataSource dataSource;
+	private DataSource dataSource;
 	
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
@@ -57,7 +57,6 @@ public class UserDao {
 	public void deleteAll() throws SQLException {
 		Connection c = dataSource.getConnection();
 		
-<<<<<<< Upstream, based on branch 'master' of https://github.com/forthenew/springbook.git
 		PreparedStatement ps = c.prepareStatement("DELETE FROM users");
 		ps.executeUpdate();
 		
@@ -79,28 +78,5 @@ public class UserDao {
 		c.close();
 		
 		return count;
-		
-=======
-		PreparedStatement ps = c.prepareStatement("DELETE FROM Users");
-		ps.executeQuery();
-		ps.close();
-		c.close();
-	}
-	
-	public int getCount() throws SQLException {
-		Connection c = dataSource.getConnection();
-		
-		PreparedStatement ps = c.prepareStatement("SELECT COUNT(*) FROM Users");
-		
-		ResultSet rs = ps.executeQuery();
-		rs.next();
-		int count = rs.getInt(1);
-		
-		rs.close();
-		ps.close();
-		c.close();
-		
-		return count;
->>>>>>> 66c6cf2 synced
 	}
 }
