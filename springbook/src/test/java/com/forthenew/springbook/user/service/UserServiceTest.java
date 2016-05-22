9f34834df53e2234819553cfb8eab8c68c4ad8ff
forthenew <forthenew@forthenew-Desktop>
2016-05-22 23:30:36 +0900

    MockUserDao를 사용해서 만든 고립된 테스트

--- springbook/src/test/java/com/forthenew/springbook/user/service/UserServiceTest.java
+++ springbook/src/test/java/com/forthenew/springbook/user/service/UserServiceTest.java
@@ -57,27 +57,36 @@ public class UserServiceTest {
 				);
 	}
 	@Test
-	@DirtiesContext
 	public void upgradeLevels() {
-		userDao.deleteAll();
+		UserServiceImpl userServiceImpl = new UserServiceImpl();
 		
-		for(User user:users) userDao.add(user);
+		MockUserDao mockUserDao = new MockUserDao(this.users);
+		userServiceImpl.setUserDao(mockUserDao);
 		
 		MockMailSender mockMailSender = new MockMailSender();
 		userServiceImpl.setMailSender(mockMailSender);
 		
-		userService.upgradeLevels();
+		userServiceImpl.upgradeLevels();
 		
-		checkLevelUpgraded(users.get(0), false);
-		checkLevelUpgraded(users.get(1), true);
-		checkLevelUpgraded(users.get(2), false);
-		checkLevelUpgraded(users.get(3), true);
-		checkLevelUpgraded(users.get(4), false);
+		List<User> updated = mockUserDao.getUpdated();
+		assertThat(updated.size(), is(2));
+		checkUserAndLevel(updated.get(0), "joytouch", Level.SILVER);
+		checkUserAndLevel(updated.get(1), "madnite1", Level.GOLD);
 		
 		List<String> request = mockMailSender.getRequests();
 		assertThat(request.size(), is(2));
 		assertThat(request.get(0), is(users.get(1).getEmail()));
 		assertThat(request.get(1), is(users.get(3).getEmail()));
+		
+//		checkLevelUpgraded(users.get(0), false);
+//		checkLevelUpgraded(users.get(1), true);
+//		checkLevelUpgraded(users.get(2), false);
+//		checkLevelUpgraded(users.get(3), true);
+//		checkLevelUpgraded(users.get(4), false);
+	}
+	private void checkUserAndLevel(User updated, String expectedId, Level expectedLevel) {
+		assertThat(updated.getId(), is(expectedId));
+		assertThat(updated.getLevel(), is(expectedLevel));
 	}
 	private void checkLevelUpgraded(User user, boolean upgraded) {
 		User userUpdate = userDao.get(user.getId());
@@ -151,6 +160,39 @@ public class UserServiceTest {
 		}
 		public void send(SimpleMailMessage[] mailMessage) throws MailException {
 			
+		}
+	}
+	static class MockUserDao implements UserDao {
+		private List<User> users;
+		private List<User> updated = new ArrayList();
+		
+		private MockUserDao(List<User> users) {
+			this.users = users;
+		}
+		public List<User> getUpdated() {
+			return this.updated;
+		}
+		public List<User> getAll() {
+			return this.users;
+		}
+		public void update(User user) {
+			updated.add(user);
+		}
+		@Override
+		public void add(User user) {
+			throw new UnsupportedOperationException();
+		}
+		@Override
+		public User get(String id) {
+			throw new UnsupportedOperationException();
+		}
+		@Override
+		public void deleteAll() {
+			throw new UnsupportedOperationException();
+		}
+		@Override
+		public int getCount() {
+			throw new UnsupportedOperationException();
 		}
 	}
 }
